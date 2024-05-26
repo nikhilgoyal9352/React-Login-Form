4f31ec91c6f49ca3fe7dcab2a9b77a679aeb0282
nikhilgoyal9352 <166325957+nikhilgoyal9352@users.noreply.github.com>
2024-05-26 23:56:47 +0530

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Form from './Form.jsx'
+import './form.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <Form />
+  </React.StrictMode>,
+)
